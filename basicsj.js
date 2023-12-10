e58c514da7ae00233cc6fb9c94e167df1c72012a
deryacoban <394795@ogr.ktu.edu.tr>
2023-12-10 23:17:13 +0300

    deneme

---
+++ basicsj.js
@@ -0,0 +1,30 @@
+
+const products = [
+    {
+        id: 1,
+        name: "Product 1",
+        description: "This is a description of product 1.",
+        price: 19.99,
+        image: "images/product1.jpg"
+    },
+    // ... other product objects
+];
+
+// Generate product list dynamically
+function renderProducts() {
+    const productList = document.querySelector(".products-list");
+    productList.innerHTML = "";
+    for (const product of products) {
+        const productItem = document.createElement("li");
+        productItem.innerHTML = `
+            <a href="product.html?id=${product.id}">
+                <img src="${product.image}" alt="${product.name}">
+                <p class="product-name">${product.name}</p>
+                <p class="product-price">$${product.price}</p>
+            </a>
+        `;
+        productList.appendChild(productItem);
+    }
+}
+
+renderProducts();
